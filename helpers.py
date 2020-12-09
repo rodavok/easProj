8541368bcdfde788ee05d00a032e2203f81e3aae
Robert O'Keeffe <rodavok@gmaik.com>
2020-12-08 20:06:26 -0500

    Combined extra functions into a single helper file and now take user-provided paths for handling files

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlite3
 import re
 from us_state_abbrev import us_state_abbrev, abbrev_us_state
 
@@ -88,7 +89,7 @@ def tweets_json_to_dfs(filepath):
 
     #dont forget to add json to gitignore
     #load data
-    tweets = pd.read_json('./data/policy.json')
+    tweets = pd.read_json(filepath)
     userraw = pd.DataFrame(tweets['user'].tolist())
 
     #users df
@@ -104,6 +105,68 @@ def tweets_json_to_dfs(filepath):
     tweets = tweets[tweets['user_id'].isin(users['id'])]
     tweets = tweets[target_tweet_cols]
 
-    print(tweets.columns)
-
     return users, tweets
+
+
+def make_database(data_source, database)
+
+    #make connection to new db
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    #load tweets
+    users, tweets = tweets_json_to_dfs(data_source)
+    c.execute("DROP TABLE IF EXISTS users")
+    c.execute("DROP TABLE IF EXISTS tweets")
+
+    users_sql = """CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    screen_name TEXT,
+    location TEXT,
+    url TEXT,
+    description TEXT,
+    protected INTEGER,
+    verified INTEGER,
+    followers_count INTEGER,
+    friends_count INTEGER,
+    favourites_count INTEGER,
+    statuses_count INTEGER,
+    created_at DATETIME,
+    utc_offset TEXT,
+    time_zone TEXT,
+    geo_enabled INTEGER,
+    lang TEXT, 
+    default_profile INTEGER,
+    default_profile_image INTEGER,
+    state INTEGER)"""
+
+    tweets_sql = """CREATE TABLE tweets (
+    created_at DATETIME,
+    id integer PRIMARY KEY,
+    user_id INTEGER,
+    text TEXT,
+    in_reply_to_status_id INTEGER,
+    in_reply_to_user_id INTEGER,
+    is_quote_status TEXT,
+    quote_count INTEGER,
+    reply_count INTEGER,
+    retweet_count INTEGER,
+    favorite_count INTEGER,
+    lang TEXT,
+    FOREIGN KEY (user_id) references users(id))"""
+
+    conn.execute(users_sql)
+    conn.execute(tweets_sql)
+    users.to_sql(name = 'users', 
+                             con = conn,
+                             if_exists = 'append', 
+                             index = False)
+
+    tweets.to_sql(name= 'tweets',
+                            con = conn,
+                            if_exists = 'append',
+                            index = False)
+
+    conn.commit()
+
+    conn.close()
