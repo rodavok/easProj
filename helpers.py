b7b5c99d68947b1a02f80b2dd34900db34b48f79
rob <rodavok@gmail.com>
2020-12-12 23:28:43 -0500

    tweets.text contains full_text if truncated, helpers.contains_state searches for abbrev, state name, and cities in states, requirements.txt should work for windows

--- helpers.py
+++ helpers.py
@@ -1,7 +1,47 @@
 import pandas as pd
 import sqlite3
 import re
+import numpy as np
 from us_state_abbrev import us_state_abbrev, abbrev_us_state
+from city_to_state import city_to_state_dict
+
+
+#helper functions
+def contains_state(loc_string):
+    '''
+    parameters:
+        loc_string: user provided location data from user['location']
+
+    returns:
+        state name if found, otherwise None
+
+    '''
+    #look for two consecutive capital letters
+    try:
+        #check if string contains a state name
+        state = [state for state in us_state_abbrev.keys() if (str.lower(state) in str.lower(loc_string))]
+        return us_state_abbrev[state[0]]
+    except:
+        pass
+    try:
+        #check if string contains a state abbreviation
+        regex = r'\b[A-W][A-Y]\b'
+        abbrev = re.search(regex, loc_string)
+        return abbrev.group()
+    except:
+        pass
+    try:
+        #check if string contains a city
+        city = [city for city in city_to_state_dict.keys() if (str.lower(city) in str.lower(loc_string))]
+        return us_state_abbrev[city_to_state_dict[city[0]]]
+    except:
+        pass
+
+def lookup(dictionary, val):
+    try:
+        return dictionary[val]
+    except:
+        return None
 
 def tweets_json_to_dfs(filepath):
     '''
@@ -12,35 +52,6 @@ def tweets_json_to_dfs(filepath):
         users: dataframe containing trimmed user data
         tweets: dataframe containing trimmed tweets
     '''
-    #helper functions
-    def contains_state(loc_string):
-        '''
-        parameters:
-            loc_string: user provided location data from user['location']
-
-        returns:
-            state name if found, otherwise None
-
-        currently I'm just looking if the abbreviation is in the location string
-        
-        would like to add checks for full state name or major cities, but 
-        this function has to return the name of the state associated with the match,
-         or None if no match is found
-
-        '''
-        #look for two consecutive capital letters
-        regex = r'\b[A-W][A-Y]\b'
-        try:
-            x = re.search(regex, loc_string)
-            return x.group()
-        except:
-            return None
-
-    def lookup(dictionary, val):
-        try:
-            return dictionary[val]
-        except:
-            return None
 
     target_user_cols = [
         'id',
@@ -77,7 +88,7 @@ def tweets_json_to_dfs(filepath):
         'favorite_count',
         'lang']
 
-    #may be useful later for tweets-
+    #may be useful later (tweets)
     #entities
     #truncated
     #entities
@@ -87,28 +98,32 @@ def tweets_json_to_dfs(filepath):
     #extended tweet
 
 
-    #dont forget to add json to gitignore
     #load data
     tweets = pd.read_json(filepath)
     userraw = pd.DataFrame(tweets['user'].tolist())
 
     #users df
+    #drop duplicate users 
     users = userraw.drop_duplicates(subset=['id'])[target_user_cols]
-    #I can write this better
+    #determine state by location
     users['state'] = [lookup(abbrev_us_state, abbrev) for abbrev in [contains_state(location) for location in users['location']]]
     #drop the users whose state we couldn't determine (to minimize db filesize)
     users = users.dropna(subset=['state'])
 
     #tweets df
+    #add user ids
     tweets['user_id'] = userraw['id']
     #drop tweets from users whose state we couldn't determine
     tweets = tweets[tweets['user_id'].isin(users['id'])]
+    #replace text with full text if the tweet is an extended tweet
+    tweets['full_text'] = [d.get('full_text') if type(d) == dict else None for d in tweets['extended_tweet']]
+    tweets['text'] = np.where(tweets['truncated'], tweets['full_text'], tweets['text'])
     tweets = tweets[target_tweet_cols]
 
     return users, tweets
 
 
-def make_database(data_source, database)
+def make_database(data_source, database):
 
     #make connection to new db
     conn = sqlite3.connect(database)
